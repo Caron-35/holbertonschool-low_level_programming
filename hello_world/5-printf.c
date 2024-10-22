9cf77c41970046c469464534ef2fc785a5760fc0
Caron-35 <10070@holbertonstudents.com>
2024-10-22 11:45:09 +0000

    5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry of the program
+*
+* printf: fonction pour ecrire le programme
+* return: toujours 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
