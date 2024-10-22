1c8d15967a7a7c42ef48e6cc7de4a12ce20a8b76
Caron-35 <10070@holbertonstudents.com>
2024-10-22 11:22:01 +0000

    4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
- * return: Always 0 (Success)
+ * Description: This program prints a specific phrase.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
