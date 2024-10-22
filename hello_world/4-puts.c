ead2fcba0054db3450695527e1698f8b3c17ba78
Caron-35 <10070@holbertonstudents.com>
2024-10-22 10:33:06 +0000

    4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#!/bin/bash
-gcc 4-puts.c -o 4-puts_0
-int main(){
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
-   }
+#include <stdio.h>
+
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
