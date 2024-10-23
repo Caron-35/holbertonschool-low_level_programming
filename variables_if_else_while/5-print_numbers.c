a714c58f471b3abf9f6f39d008b9d977c59e14b8
Caron-35 <10070@holbertonstudents.com>
2024-10-23 09:25:39 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Generates all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned int x = 0;
+while (x > 10)
+{
+printf("%u\n", x);
+x++;
+}
+return (0);
+}
