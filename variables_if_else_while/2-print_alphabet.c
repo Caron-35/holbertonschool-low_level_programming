0b0383d0d1b000a33fbf0728800457f7a2db89bf
Caron-35 <10070@holbertonstudents.com>
2024-10-23 08:06:52 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ *
+ *
+ *
+ * Return to 0
+ */
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
