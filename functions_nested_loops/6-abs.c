855d48e3da52cb603b03503fbd32f79641e16763
Caron-35 <10070@holbertonstudents.com>
2024-10-25 14:42:18 +0200

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Program the absolute number
+ * @int: number variable
+ * r : number
+ * Return: always the absolute number
+ */
+int _abs(int)
+{
+int r = n;
+if (r < 0)
+{
+return (-r);
+}
+else
+{
+return (r);
+}
+}
