1d1fff422e84df6adf981e9a4553bedc6532024a
Caron-35 <10070@holbertonstudents.com>
2024-10-25 18:05:30 +0200

    10-add.c

--- functions_nested_loops/10-main.c
+++ functions_nested_loops/10-main.c
@@ -5,16 +5,11 @@
  *
  * Return: Always 0.
  */
-int add(int a, int b)
-{
-  return (a + b);
-}
-
 int main(void)
 {
     int n;
 
-    n = add(89, 9);
+    n = 89 + 9;
     printf("%d\n", n);
     return (0);
 }
