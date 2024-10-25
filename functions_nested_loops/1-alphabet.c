84ff92d1f64c8be3e2ebf1f2cc5b30e8dfdbcbcb
Caron-35 <10070@holbertonstudents.com>
2024-10-25 10:34:20 +0200

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Generates a program to print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+return (0);
+}
+
+  
