f779a34065d1542a71ffdb13f0ace2d13bcd0b80
Caron-35 <10070@holbertonstudents.com>
2024-10-25 16:58:42 +0200

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,11 +3,12 @@
 void times_table(void)
 {
     int i, j;
+    int f = i*j;
     for (i = 0; i <= 9; i++)
     {
         for (j = 0; j <= 9; j++)
         {
-	  _putchar( i * j);
+	  _putchar(f);
             if (j < 9)
             {
 	      _putchar(',');
