a618a91d7aea786664ac9f388469a5138a913da7
Caron-35 <10070@holbertonstudents.com>
2024-10-25 16:56:59 +0200

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/***/
+void times_table(void)
+{
+    int i, j;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+	  _putchar( i * j);
+            if (j < 9)
+            {
+	      _putchar(',');
+	      _putchar(' ');
+            }
+        }
+        _putchar("\n");
+    }
+    return (0);
+}
