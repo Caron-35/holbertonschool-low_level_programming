c83aa1be6fa3d8fb988d7a86b37b20bdb2c3198e
Caron-35 <10070@holbertonstudents.com>
2024-10-25 15:43:33 +0200

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - program d'une journee
+ * @h: heure
+ * @m: minute
+ * No return
+ */
+void jack_bauer(void)
+{
+int h = 0;
+int m = 0;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar(h / 10);
+_putchar(h % 10);
+_putchar(':');
+_putchar(m / 10);
+_putchar(m % 10);
+_putchar('\n');
+}
+}
+}
+      
