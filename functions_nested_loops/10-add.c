7e0a1f3249218ef6cb10cd8743138c296785bda4
Caron-35 <10070@holbertonstudents.com>
2024-10-25 17:36:22 +0200

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * print_to_98 - print to 98
  *
@@ -7,13 +8,34 @@
 void print_to_98(int n) {
     if (n <= 98) {
         for (int i = n; i < 98; i++) {
-            printf("%d, ", i);
+            // Print the number
+            if (i < 10) {
+                _putchar(i + '0'); // Handle single digits
+            } else {
+                _putchar((i / 10) + '0'); // Print tens place
+                _putchar((i % 10) + '0'); // Print units place
+            }
+            _putchar(',');
+            _putchar(' ');
         }
-        printf("98\n");
+        // Print 98 without a trailing comma
+        _putchar('9');
+        _putchar('8');
     } else {
         for (int i = n; i > 98; i--) {
-            printf("%d, ", i);
+            // Print the number
+            if (i < 10) {
+                _putchar(i + '0'); // Handle single digits
+            } else {
+                _putchar((i / 10) + '0'); // Print tens place
+                _putchar((i % 10) + '0'); // Print units place
+            }
+            _putchar(',');
+            _putchar(' ');
         }
-        printf("98\n");
+        // Print 98 without a trailing comma
+        _putchar('9');
+        _putchar('8');
     }
+    _putchar('\n'); // New line at the end
 }
