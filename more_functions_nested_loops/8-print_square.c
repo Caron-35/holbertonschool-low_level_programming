4588ba86829738c094cb816c3dc1d00d26c823d4
Caron-35 <10070@holbertonstudents.com>
2024-10-28 13:44:01 +0100

    7-print_diagonal.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_square - Print square
+ * @size: size
+ */
+void print_square(int size)
+{
+int i, j;
+for (idx1 = 0; idx1 < size; i++)
+{
+for (idx2 = 0; idx2 < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
