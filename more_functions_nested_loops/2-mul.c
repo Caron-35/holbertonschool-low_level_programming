55eb623de8e50c033c2c97a9628526fa2cb3e5f0
Caron-35 <10070@holbertonstudents.com>
2024-10-28 10:16:34 +0100

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Generate a multiplication
+ * @a : first number
+ * @b : second number
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
