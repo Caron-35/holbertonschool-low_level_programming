219e72217548a1afab44c108f77c184132b3890e
Caron-35 <10070@holbertonstudents.com>
2024-10-28 14:48:19 +0100

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_triangle - Print a triangle
+ */
+void print_triangle(int size)
+{
+int i, j, k;
+for (i = 0; i <= size; i++)
+{
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
