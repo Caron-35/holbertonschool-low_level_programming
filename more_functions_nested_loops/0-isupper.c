d6a932b697daf0f27a308beda78a2fddb72f5610
Caron-35 <10070@holbertonstudents.com>
2024-10-28 09:52:26 +0100

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Generate a program that check if it's uppercase
+ * @c : character
+ * Return: 1 if uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
