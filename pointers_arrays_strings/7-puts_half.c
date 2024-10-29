4fa870abb5a69167e018d2aaa3331db31f361281
Caron-35 <10070@holbertonstudents.com>
2024-10-29 14:32:34 +0100

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ int i;
 while (str[length] != '\0')
 {
 length++;
-}    
+}
 if (length % 2 == 0)
 {
 start = length / 2;
@@ -23,6 +23,6 @@ start = (length / 2) + 1;
 for (i = start; i < length; i++)
 {
 _putchar(str[i]);
-}    
+}
 _putchar('\n');
 }
