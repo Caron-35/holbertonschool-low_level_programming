f19be88e11c40acfa09b9e64158a462fac7dd1d9
Caron-35 <10070@holbertonstudents.com>
2024-10-29 11:14:13 +0100

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,11 +4,12 @@ void rev_string(char *s)
 {
 int len = 0;
 char temp;
+int i;
 while (s[len] != '\0')
 {
 len++;
 }
-for (int i = 0; i < len / 2; i++)
+for (i = 0; i < len / 2; i++)
 {
 temp = s[i];
 s[i] = s[len - 1 - i];
