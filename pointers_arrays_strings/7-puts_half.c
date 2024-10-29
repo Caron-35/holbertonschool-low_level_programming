6b7718e37f9e9b754f084dc49e70ba2e91365f99
Caron-35 <10070@holbertonstudents.com>
2024-10-29 14:26:55 +0100

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,18 +5,16 @@
  */
 void puts_half(char *str)
 {
-int len = _strlen(str);
-char *ptr = str;
-if (len % 2 == 0) {
-ptr += len / 2;
-}
-else
+int length = 0;
+int i;
+while (str[length] != '\0')
 {
-ptr += (len / 2) + 1;
+length++;
 }
-while (*ptr != '\0') {
-_putchar(*ptr);
-ptr++;
-}
+int start = (length % 2 == 0) ? (length / 2) : (length / 2 + 1);
+for (i = start; i < length; i++)
+{
+_putchar(str[i]);
+}   
 _putchar('\n');
 }
