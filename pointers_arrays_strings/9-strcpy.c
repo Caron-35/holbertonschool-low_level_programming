1ef61ceaa41672dcf82661c418b770818f87cc74
Caron-35 <10070@holbertonstudents.com>
2024-10-29 15:23:07 +0100

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copy the string pointed by src
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int *temp = dest;
+while (*src != '\0')
+{
+*temp++ = *src;
+}
+*temp = '\0';
+return (dest);
+}
