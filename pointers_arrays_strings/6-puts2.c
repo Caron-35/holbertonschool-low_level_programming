793a6c1612c87e970ae4f694962eb870507fa995
Caron-35 <10070@holbertonstudents.com>
2024-10-29 13:37:59 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,11 @@
  */
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i += 2)
+char *ptr = str;
+while (*ptr != '\0')
 {
-_putchar(str[i]);
+_putchar(*ptr);
+ptr += 2;
 }
 _putchar('\n');
 }
