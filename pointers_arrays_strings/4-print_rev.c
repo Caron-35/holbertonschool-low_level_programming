c31098020d89916791c78e4b91f050a7fab33bb0
Caron-35 <10070@holbertonstudents.com>
2024-10-29 10:26:22 +0100

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - Print a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+int i = 0;
+int len = _strlen(s);
+for (i = len - 1; i >=0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
