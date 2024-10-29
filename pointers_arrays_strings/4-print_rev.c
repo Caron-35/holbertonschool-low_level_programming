36a85391c5f6f1affb1184e67589ab80bd63cf4d
Caron-35 <10070@holbertonstudents.com>
2024-10-29 10:47:16 +0100

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,19 @@
  * print_rev - Print a string in reverse
  * @s: string
  */
+int _strlen(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+return len;
+}
 void print_rev(char *s)
 {
 int i = 0;
-int len = 0;
-while (s[len] != '\0')
-{len ++;
-}
-return (len);
+int len = _strlen(s);
 for (i = len - 1; i >=0; i--)
 {
 _putchar(s[i]);
