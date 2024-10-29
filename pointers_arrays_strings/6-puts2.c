2dde8485df6f8154039c892fa866ede4deb51bba
Caron-35 <10070@holbertonstudents.com>
2024-10-29 13:53:40 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,9 +3,15 @@
  * puts2 - Print every other character
  * @str: string
  */
-void puts2(char *str) {
+void puts2(char *str)
+{
 char *ptr = str;
-while (*ptr != '\0') {
+while (*ptr != '\0')
+{
+if (*ptr == 'H')
+{
+break;
+}
 _putchar(*ptr);
 ptr += 2;
 }
