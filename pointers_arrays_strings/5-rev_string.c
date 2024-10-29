d115441b1705b1681983705da9082b32542cbe63
Caron-35 <10070@holbertonstudents.com>
2024-10-29 13:33:42 +0100

    6-puts2.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -19,3 +19,26 @@ s[i] = s[len - 1 - i];
 s[len - 1 - i] = temp;
 }
 }
+/*
+  void rev_string(char *s)
+{
+char *start = s;            // Pointeur vers le début de la chaîne
+char *end = s;              // Pointeur vers la fin de la chaîne
+char temp;
+// Déplacer le pointeur `end` à la fin de la chaîne
+ while (*end != '\0') {
+end++;
+}
+end--; // Ajuster pour pointer sur le dernier caractère avant
+le terminateur nul
+
+// Échanger les caractères en place
+while (start < end) {
+temp = *start;
+*start = *end;
+*end = temp;
+start++;               // Avancer le pointeur `start`
+end--;                 // Reculer le pointeur `end`
+}
+}
+*/
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - Print every other character
+ * @str: string
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
