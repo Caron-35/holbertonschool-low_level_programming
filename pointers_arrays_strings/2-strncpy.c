bf7a6b0b424d81b8fdab3b0eb2ecee6941009c08
Caron-35 <10070@holbertonstudents.com>
2024-10-30 10:34:08 +0100

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - Copy a string and stop at n point
+ * @dest: destination
+ * @src: source
+ * @n: nombre de caracteres a copier
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char *temp = dest;
+while (n-- && *src)
+{
+*temp++ = *src++;
+}
+while (n-- > 0)
+{
+*temp++ = '\0';
+}
+return (dest);
+}
