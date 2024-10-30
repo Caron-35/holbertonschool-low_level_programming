f07a435c68754cb6bb192a30b119c1c75be3de0b
Caron-35 <10070@holbertonstudents.com>
2024-10-30 13:44:38 +0100

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: number
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int temp = a[i];
+for (i = 0; i < n/2; i++)
+{
+temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
