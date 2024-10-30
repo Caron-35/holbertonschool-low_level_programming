66ab09f24772591929fbc049729ca50277c2e9e1
Caron-35 <10070@holbertonstudents.com>
2024-10-30 14:40:17 +0100

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
-/***/
+/**
+ * string_toupper - Convert string to uppercase
+ * @str: string
+ * Return: return the string in uppercase
+ * Description - Check each character if it's lowercase
+ * then convert and increment for check the next character
+ */
 char *string_toupper(char *str)
 {
 char *original = str;
@@ -9,7 +15,7 @@ if (*str >= 'a' && *str <= 'z')
 {
 *str = *str - ('a' - 'A');
 }
- str++;
+str++;
 }
 return (original);
 }
