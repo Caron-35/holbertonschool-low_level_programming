522e7e99790eea114b0ae5aaddefd01561942bfb
Caron-35 <10070@holbertonstudents.com>
2024-10-31 15:57:02 +0100

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _atoi - Generates the function atoi
- * @s: string
+ * _atoi - Converts a string to an integer
+ * @s: input string
  * Return: sign * result
  */
 int _atoi(char *s)
 {
 int result = 0;
 int sign = 1;
- int verif = 0;
+int verif = 0;
 while (*s == ' ')
 {
 s++;
@@ -18,15 +18,19 @@ if (*s == '-')
 sign = -1;
 s++;
 }
-if (*s >= '0' && *s <= '9')
+else if (*s == '+')
 {
-verif = 1
+s++;
+}
+while (*s >= '0' && *s <= '9')
+{
+verif = 1;
 result = result * 10 + (*s - '0');
 s++;
 }
- if else ( verif = 1)
+if (!verif)
 {
-break;
+return (0);
 }
 return (sign * result);
 }
